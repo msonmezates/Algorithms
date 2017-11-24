860e201383bcea6d2f7515b8939678fc833febf4
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-24 17:46:07 -0600

    Add alternative solution

--- AlgoCasts-master/exercises/reversestring/index.js
+++ AlgoCasts-master/exercises/reversestring/index.js
@@ -6,14 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+// function reverse(str) {
+//   let strArr = str.split('');
+//   let result = [];
+//   for(let i = strArr.length - 1; i>=0; i--) {
+//     result.push(strArr[i]);
+//   }
+//   result = result.join('');
+//   return result
+// }
+//
+// module.exports = reverse;
+
+//Alternative solution
 function reverse(str) {
-  let strArr = str.split('');
-  let result = [];
-  for(let i = strArr.length - 1; i>=0; i--) {
-    result.push(strArr[i]);
-  }
-  result = result.join('');
-  return result
+  return str.split('').reverse().join('');
 }
 
 module.exports = reverse;
