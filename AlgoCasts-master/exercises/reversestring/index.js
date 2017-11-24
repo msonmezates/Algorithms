289f4486e838af801b63b56187eb07ea6b5e44eb
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-24 17:29:45 -0600

    Add a solution

--- AlgoCasts-master/exercises/reversestring/index.js
+++ AlgoCasts-master/exercises/reversestring/index.js
@@ -6,6 +6,14 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let strArr = str.split('');
+  let result = [];
+  for(let i = strArr.length - 1; i>=0; i--) {
+    result.push(strArr[i]);
+  }
+  result = result.join('');
+  return result
+}
 
 module.exports = reverse;
