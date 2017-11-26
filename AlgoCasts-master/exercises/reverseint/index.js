c52ae165b3d4949ea64db66428a3abdfe820e92f
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-25 21:50:49 -0600

    Add a solution for reverseInt algorithm

--- AlgoCasts-master/exercises/reverseint/index.js
+++ AlgoCasts-master/exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const reversed = n.toString().split('').reduce((arr, char) => {
+    return char + arr;
+  }, '');
+  if (n<0) return parseInt(reversed) * -1;
+  else return parseInt(reversed);
+}
 
 module.exports = reverseInt;
