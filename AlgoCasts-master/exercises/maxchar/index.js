6e993f3c4ddacf7b88d6b8da6d3b46630c4a3a80
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-26 11:01:44 -0600

    Add a solution for maxchar algorithm

--- AlgoCasts-master/exercises/maxchar/index.js
+++ AlgoCasts-master/exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {};
+  let maxFrequency = 0;
+  let maxChar = '';
+  for(let char of str) {
+    !obj[char] ? obj[char] = 1 : obj[char]++;
+  }
+  for(let char in obj) {
+    if(obj[char] > maxFrequency) {
+      maxFrequency = obj[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
