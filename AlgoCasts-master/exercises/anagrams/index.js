def3105a269f29f39a466074f66bb3372fd10392
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-30 21:23:14 -0600

    Add an alternate solution for anagrams

--- AlgoCasts-master/exercises/anagrams/index.js
+++ AlgoCasts-master/exercises/anagrams/index.js
@@ -9,29 +9,51 @@
 //   anagrams('Hi there', 'Bye there') --> False
 
 function anagrams(stringA, stringB) {
-  const stringA1 = stringA.replace(/[^\w]/g, "").toLowerCase();
-  const stringB1 = stringB.replace(/[^\w]/g, "").toLowerCase();
+  const myObjA = myMapObj(stringA);
+  const myObjB = myMapObj(stringB);
 
-  const objA = {};
-  const objB = {};
-  for(let char of stringA1) {
-    if(!objA[char]) objA[char] = 1;
-    else objA[char]++;
+  if(Object.keys(myObjA).length !== Object.keys(myObjB).length) return false;
+
+  for(let char in myObjA) {
+    if(myObjA[char] !== myObjB[char]) return false;
   }
-  for(let char of stringB1) {
-    if(!objB[char]) objB[char] = 1;
-    else objB[char]++;
+  return true;
+}
+
+function myMapObj(string) {
+  let myObj = {};
+  for(let char of string.replace(/[^\w]/g, "").toLowerCase()) {
+    myObj[char] = myObj[char] + 1 || 1;
   }
-  if(Object.keys(objA).length !== Object.keys(objB).length) {
-    return false;
-  }
-  for(let char in objA) {
-    if(objA[char] !== objB[char]) {
-      return false;
-    } else {
-      return true;
-    }
-  }
+  return myObj
 }
 
 module.exports = anagrams;
+
+// --------------------------
+// Alternate solution
+// function anagrams(stringA, stringB) {
+//   const stringA1 = stringA.replace(/[^\w]/g, "").toLowerCase();
+//   const stringB1 = stringB.replace(/[^\w]/g, "").toLowerCase();
+//
+//   const objA = {};
+//   const objB = {};
+//   for(let char of stringA1) {
+//     if(!objA[char]) objA[char] = 1;
+//     else objA[char]++;
+//   }
+//   for(let char of stringB1) {
+//     if(!objB[char]) objB[char] = 1;
+//     else objB[char]++;
+//   }
+//   if(Object.keys(objA).length !== Object.keys(objB).length) {
+//     return false;
+//   }
+//   for(let char in objA) {
+//     if(objA[char] !== objB[char]) {
+//       return false;
+//     } else {
+//       return true;
+//     }
+//   }
+// }
