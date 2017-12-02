e976ba9aa6732ebfa79071cea42871537a209908
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-02 15:30:19 -0600

    Add an alternate solution

--- AlgoCasts-master/exercises/pyramid/index.js
+++ AlgoCasts-master/exercises/pyramid/index.js
@@ -14,19 +14,40 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  for(let row=0; row<n; row++) {
-    let str = '';
-    for(let col=0; col<(n*2-1); col++) {
-      const midPoint = Math.floor((n*2-1)/2);
-      if(midPoint-row <= col && midPoint+row >= col) {
-        str += '#';
-      } else {
-        str += ' ';
-      }
-    }
-    console.log(str);
+function pyramid(n, row=0, str='') {
+  if(row === n) {
+    return;
   }
+
+  if(str.length === 2*n-1) {
+    console.log(str);
+    return pyramid(n, row+1);
+  }
+  const midPoint = Math.floor((n*2-1)/2);
+  let add;
+  if(midPoint-row <= str.length && midPoint+row >= str.length) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  return pyramid(n, row, str+add);
 }
 
 module.exports = pyramid;
+
+// ----------------------
+// Alternate solution
+// function pyramid(n) {
+//   for(let row=0; row<n; row++) {
+//     let str = '';
+//     for(let col=0; col<(n*2-1); col++) {
+//       const midPoint = Math.floor((n*2-1)/2);
+//       if(midPoint-row <= col && midPoint+row >= col) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       }
+//     }
+//     console.log(str);
+//   }
+// }
