e7fc890f2bca0ca7dd074c403ed038d34d4c6d80
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-02 23:01:09 -0600

    Add a solution for vowels algorithm

--- AlgoCasts-master/exercises/vowels/index.js
+++ AlgoCasts-master/exercises/vowels/index.js
@@ -7,6 +7,16 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  str = str.toLowerCase();
+  let sum = 0;
+  const vowel = 'aeiou';
+  for(let char of str) {
+    if(vowel.indexOf(char) >= 0) {
+      sum++;
+    }
+  }
+  return sum;
+}
 
 module.exports = vowels;
