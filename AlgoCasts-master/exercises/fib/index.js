0aa512f008d5a3683f5fc75660e69f5bf9b65243
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-12-07 20:50:11 -0600

    Add memoization for fibonacci algorithm

--- AlgoCasts-master/exercises/fib/index.js
+++ AlgoCasts-master/exercises/fib/index.js
@@ -8,10 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {
- if(n<2) return n;
- return fib(n-1) + fib(n-2);
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if(cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
 }
+
+function slowFib(n) {
+  if(n<2) {
+    return n;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
 
@@ -24,4 +42,12 @@ module.exports = fib;
 //     result.push(num);
 //   }
 //   return result[n];
+// }
+
+
+// --------------------------------
+// Alternate solution
+// function fib(n) {
+//  if(n<2) return n;
+//  return fib(n-1) + fib(n-2);
 // }
